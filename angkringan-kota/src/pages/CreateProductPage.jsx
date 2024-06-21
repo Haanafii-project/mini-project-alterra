84d507552bf4e6d3b8a4664d7bbc83383f1af305
Hanafi Afwan Syahnuri <hanafi.afwan.syahnuri@gmail.com>
2024-06-21 15:47:10 +0700

    Update CreateProductPage.jsx

--- angkringan-kota/src/pages/CreateProductPage.jsx
+++ angkringan-kota/src/pages/CreateProductPage.jsx
@@ -6,58 +6,76 @@ import { useState, useEffect } from 'react';
 
 
 export default function CreateProductPage() {
-    
-    const [editId, setEditId] = useState();
-    const [nameProduct, setNameProduct] = useState ("");
-    const [descriptionProduct, setDescriptionProduct] = useState ("");
-    const [priceProduct, setPriceProduct] = useState ("");
-    const [product, setProduct] = useState ([]);
-    const [image, setImage] = useState(null);
-    const [response, setResponse] = useState();
-    
-    async function getProduct () {
-        const response = await axios.get("https://663b2a3dfee6744a6ea08fb2.mockapi.io/product");
-        setProduct(response.data);
-    }
 
-    useEffect(() => {
-        getProduct()
+	const [product, setProduct] = useState ([]); // Variabel state untuk menyimpan daftar produk.
+	const [nameProduct, setNameProduct] = useState (""); // Variabel state untuk menyimpan nama produk.
+	const [descriptionProduct, setDescriptionProduct] = useState ("");  // Variabel state untuk menyimpan deskripsi produk.
+	const [priceProduct, setPriceProduct] = useState (""); // Variabel state untuk menyimpan harga produk.
+  const [editId, setEditId] = useState(); // Variabel state untuk menyimpan ID produk yang sedang diedit 
+	
+  async function getProduct () {
+		// untuk mengambil data produk dari API menggunakan axios.
+      const response = await axios.get("https://663b2a3dfee6744a6ea08fb2.mockapi.io/product");
+      setProduct(response.data);
+  }
+
+	// useEffect berfungsi untuk menjalankan getProduct hanya sekali setelah komponen pertama kali dirender.digunakan untuk mengambil data awal yang dibutuhkan komponen untuk ditampilkan.
+	useEffect(() => {
+      getProduct()
     }, []);
-    console.log(product)
 
+
+		// Fungsi untuk Menambah Data Produk
     const addData = (e) => {
         e.preventDefault();
+				// Fungsi ini mengecek terlebih dahulu apakah terdapat nilai pada state editId
         if (editId) {
+					// Digunakan untuk mengirimkan request PUT ke API untuk memperbarui data produk yang sudah ada.
             axios.put("https://663b2a3dfee6744a6ea08fb2.mockapi.io/product/" + editId, {
                 nameProduct: nameProduct,
                 descriptionProduct: descriptionProduct,
                 priceProduct: priceProduct,
             })
+						// setelah update data selesai, state editId di-set menjadi null untuk menandakan tidak ada produk yang sedang diedit.
             setEditId(null)
         } else {
+					// Digunakan untuk mengirimkan request POST ke API untuk menambah data produk baru.
             axios.post("https://663b2a3dfee6744a6ea08fb2.mockapi.io/product", {
                 nameProduct: nameProduct,
                 descriptionProduct: descriptionProduct,
                 priceProduct: priceProduct,
             })
+
+						// Setelah menambah data, state nameProduct, descriptionProduct, dan priceProduct di-set menjadi kosong untuk persiapan penambahan data baru selanjutnya.
             setNameProduct("")
             setDescriptionProduct("")
             setPriceProduct("")
         }
+				
+				// untuk memperbarui data produk yang ditampilkan di komponen React dengan data terbaru dari API.
             getProduct()
     }
 
 
-
+		// Mendeklarasikan fungsi "deleteData" yang menerima parameter "id". Parameter ini mewakili "iD" dari produk yang ingin dihapus.
     const deleteData = (id) => {
+			// Digunakan untuk mengirimkan request DELETE ke API untuk menghapus data produk.
         axios.delete("https://663b2a3dfee6744a6ea08fb2.mockapi.io/product/" + id)
     }
 
+
+		// Mendeklarasikan fungsi editData yang menerima parameter id. Parameter ini mewakili ID dari produk yang ingin diedit.
     const editData = (id) => {
+
+			// Baris ini menggunakan fungsi find pada array product untuk mencari produk yang memiliki ID sesuai dengan parameter id.
         const editproduct = product.find(p => p.id === id)
+
+				// ika produk dengan ID yang sesuai ditemukan kode ini akan mengisi state terkait form edit
         setNameProduct(editproduct.nameProduct)
         setDescriptionProduct(editproduct.descriptionProduct)
         setPriceProduct(editproduct.priceProduct)
+
+				// Baris ini mengisi state editId dengan nilai id produk yang sedang diedit.
         setEditId(id) 
         console.log(editproduct) 
     }
@@ -115,10 +133,10 @@ export default function CreateProductPage() {
                             />
                     </div>
                 </div>
-                                <div className="my-5">
-                    <button type="submit" className="btn" onClick={addData}> 
-                        Submit
-                    </button>
+                <div className="my-5">
+                  <button type="submit" className="btn" onClick={addData}> 
+                      Submit
+                  </button>
                 </div>
             </form>
         </div>
@@ -137,15 +155,20 @@ export default function CreateProductPage() {
                     </tr>
                 </thead>
                 <tbody>
+
+										{/* Fungsi method map pada JavaScript adalah untuk membuat array baru */}
                     {product.map((item) => (
+											// Digunakan untuk mengidentifikasi dan melacak perubahan pada data produk.
                         <tr key={item.id}>
-                            <td>{item.nameProduct}</td>
-                            <td>{item.descriptionProduct}</td>
-                            <td>{item.priceProduct}</td>
-                            <td><img src={item.imageProduct} alt="" className=" w-20 h-20"/></td>
+                            <td>{item.nameProduct}</td> {/* Menampilkan nama produk. */}
+                            <td>{item.descriptionProduct}</td> {/* Menampilkan deskripsi produk. */}
+                            <td>{item.priceProduct}</td> {/* Menampilkan harga produk. */}
+                            <td><img src={item.imageProduct} alt="" className=" w-20 h-20"/></td> {/* Menampilkan gambar produk dengan elemen <img>. */} 
                             <td>
+														{/* Tombol "Delete" dengan event handler onClick yang memanggil fungsi deleteData(item.id)  */}
                             <button className="btn btn-link text-red-600" onClick={() => deleteData (item.id)}>Delete</button>
-                            <button className="btn btn-link text-red-600" onClick={() => editData (item.id)}>Edit</button>
+                            {/* Tombol "Edit" dengan event handler onClick yang memanggil fungsi editData(item.id). */}
+														<button className="btn btn-link text-red-600" onClick={() => editData (item.id)}>Edit</button>
                             </td>
                         </tr>
                     ))} 
